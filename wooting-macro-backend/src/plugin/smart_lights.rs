0c394b6d1d66aa95df80f022a956f69139ded84d
medzernik <medzernik1@gmail.com>
2022-12-29 22:49:06 +0100

    wip designing

--- wooting-macro-backend/src/plugin/smart_lights.rs
+++ wooting-macro-backend/src/plugin/smart_lights.rs
@@ -5,7 +5,10 @@ use lifx_rs;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Hash, Eq)]
 /// Phillips hue lights. Not done yet.
-pub enum PhillipsHueStatus {}
+pub struct PhillipsHueStatus{
+    pub bridge: hueclient::Bridge,
+}
+pub struct LifxStatus{}
 
 pub trait LightActions {
     fn list_lights() {}
@@ -17,15 +20,18 @@ pub trait LightActions {
     fn first_time_setup() {}
 }
 
-pub async fn phillips_first_time_setup(state: &super::super::MacroBackend) {
-    //! TODO: we need to inform the user of this and have them press the button on the bridge.
-    // TODO: Cannot test this without a bridge.
-    // TODO: Make a manual IP address setup and also an automatic one.
-    let bridge = hueclient::Bridge::discover_required()
-        .register_user(&state.config.read().await.computer_hostname.as_str())
-        .unwrap();
-
-    //println!("the username was {}", state.read().await); // handy for later
-
-    //println!("the username was {}", bridge.username); // handy for later
+impl LightActions for PhillipsHueStatus {
+    async fn first_time_setup(state: &super::super::MacroBackend) {
+        //! TODO: we need to inform the user of this and have them press the button on the bridge.
+        // TODO: Cannot test this without a bridge.
+        // TODO: Make a manual IP address setup and also an automatic one.
+        let bridge = hueclient::Bridge::discover_required()
+            .register_user(&state.config.read().await.computer_hostname.as_str())
+            .unwrap();
+    
+        //println!("the username was {}", state.read().await); // handy for later
+    
+        //println!("the username was {}", bridge.username); // handy for later
+    }
 }
+
