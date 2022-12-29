89b96e6ebb75cd1746dd751554732dd6fe55991d
medzernik <medzernik1@gmail.com>
2022-12-29 21:59:56 +0100

    added libraries and began initial draft of functions

--- wooting-macro-backend/src/plugin/smart_lights.rs
+++ wooting-macro-backend/src/plugin/smart_lights.rs
@@ -1,3 +1,23 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Hash, Eq)]
 /// Phillips hue lights. Not done yet.
 pub enum PhillipsHueStatus {}
+
+
+
+
+pub trait LightActions {
+    pub fn list_lights(){},
+    pub fn turn_on(){},
+    pub fn turn_off(){},
+    pub fn set_brightness(){},
+    pub fn set_color(){},
+    pub fn first_time_setup(){},
+}
+
+pub fn phillips_first_time_setup() {
+    let bridge = hueclient::Bridge::discover_required()
+    .register_user("mycomputer") // Press the bridge before running this
+    .unwrap();
+    println!("the username was {}", bridge.username); // handy for later
+}
+
